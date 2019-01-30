f3c32693976dce4e2bc457963ea56820dc2ba396
Мухамедин Алишер Серикович <@corp.itstep>
2019-01-30 21:12:06 +0600

    InitialCreate

---
+++ Test_1LWPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_1LWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Создать WPF приложение, в котором подключен Material Design.
+
+        //Используя компоненты material design создать в центре экрана карточку, 
+        //в которой последовательно выводится информация о вас, как о человеке в виде текста.
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click");
+        }
+    }
+}
